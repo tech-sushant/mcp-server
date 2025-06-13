c7e66a79d9e2acdcbfa93456b03545ecb8eaa28f
Ruturaj-Browserstack <ruturaj.s@browserstack.com>
2025-06-13 13:04:38 +0530

    feat: integrate browserstackLocalOptions into local binary setup

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,41 @@
+// List of supported BrowserStack Local option names (as per CLI/API)
+const BROWSERSTACK_LOCAL_OPTION_KEYS = [
+  "proxyHost",
+  "proxyPort",
+  "proxyUser",
+  "proxyPass",
+  "useCaCertificate",
+  "localProxyHost",
+  "localProxyPort",
+  "localProxyUser",
+  "localProxyPass",
+  "pacFile",
+  "force",
+  "forceLocal",
+  "onlyAutomate",
+  "verbose",
+  "logFile",
+  "binarypath",
+  "f",
+  "excludeHosts",
+];
+
+// Build browserstackLocalOptions from individual env vars
+const browserstackLocalOptions: Record<string, any> = {};
+for (const key of BROWSERSTACK_LOCAL_OPTION_KEYS) {
+  // Env var name: BROWSERSTACK_LOCAL_OPTION_<UPPERCASE_KEY>
+  const envVar = process.env[`BROWSERSTACK_LOCAL_OPTION_${key.toUpperCase()}`];
+  if (envVar !== undefined) {
+    browserstackLocalOptions[key] = envVar;
+  }
+}
+
 export class Config {
   constructor(
     public readonly browserstackUsername: string,
     public readonly browserstackAccessKey: string,
     public readonly DEV_MODE: boolean,
+    public readonly browserstackLocalOptions: Record<string, any>,
   ) {}
 }
 
@@ -10,6 +43,7 @@ const config = new Config(
   process.env.BROWSERSTACK_USERNAME!,
   process.env.BROWSERSTACK_ACCESS_KEY!,
   process.env.DEV_MODE === "true",
+  browserstackLocalOptions,
 );
 
 export default config;
