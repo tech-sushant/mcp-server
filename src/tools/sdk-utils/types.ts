170412a2de8f21d7958a5ef6ec78e9f98ab7cadb
tech-sushant <sushant.s@browserstack.com>
2025-08-07 12:38:07 +0530

    Update BrowserStack test instructions and add WebdriverIO support

--- src/tools/sdk-utils/types.ts
+++ src/tools/sdk-utils/types.ts
@@ -10,6 +10,7 @@ export enum SDKSupportedBrowserAutomationFrameworkEnum {
   playwright = "playwright",
   selenium = "selenium",
   cypress = "cypress",
+  webdriverio = "webdriverio",
 }
 export type SDKSupportedBrowserAutomationFramework =
   keyof typeof SDKSupportedBrowserAutomationFrameworkEnum;
